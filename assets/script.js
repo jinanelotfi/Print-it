435740f96922b3e30fc20e8f6ee80ee7f6b58ac6
Jinane Lotfi <jinane.lofi@gmail.com>
2023-06-30 13:47:21 +0200

    Ajout des bullet points et sélection du point en cours

--- assets/script.js
+++ assets/script.js
@@ -17,12 +17,33 @@ const slides = [
 	}
 ]
 
+// mise en place des eventListeners sur les 2 flèches
+
 let arrowLeft = document.querySelector(".arrow_left")
 	arrowLeft.addEventListener("click", ()=> {
 		console.log("test de la flèche gauche");
+
 	})
 
 let arrowRight = document.querySelector(".arrow_right")
 	arrowRight.addEventListener("click", ()=> {
 		console.log("test de la flèche droite");
 	})
+
+// mise en place des points
+
+let dotList = document.querySelector('.dots');
+
+for (let i = 0; i < slides.length; i++) {
+  let point = document.createElement('li');
+  point.classList.add('dot');
+  dotList.appendChild(point);
+}
+
+// On distingue le point en cours
+
+let firstPoint = dotList.querySelector('.dot');
+firstPoint.classList.add('dot_selected');
+
+
+	
